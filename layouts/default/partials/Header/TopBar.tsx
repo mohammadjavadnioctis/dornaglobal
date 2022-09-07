6f9a4b09ba9931d5fababd3339a0f0f7f27379a1
M.J <mohammadjavadnoctis2@gmail.com>
2022-09-07 07:59:14 +0300

    the langit status

--- layouts/default/partials/Header/TopBar.tsx
+++ layouts/default/partials/Header/TopBar.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import config from "~/app.config.js";
 import { BsTelephone } from "react-icons/bs";
 import { IoIosMail } from "react-icons/io";
+import LangSelect from "~/components/LangSelect/LangSelect";
 const TopBar = () => {
   return (
-    <div className="w-full m-auto py-1 h-[35px] bg-accent flex items-center justify-center font-sans">
-      <div className="container">
-        <div className="flex text-white items-center">
+    <div className="w-full m-auto py-1 min-h-[35px] bg-accent flex items-center justify-center font-sans">
+      <div className="container flex justify-between">
+        <div className="inline-flex text-white items-center">
           <BsTelephone className="mr-2" />
           <span className="mr-4">{config.supportPhoneNo}</span>
           <IoIosMail className="mr-2" />
           <span>{config.supportMail}</span>
+        </div>
+        <div className="inline-block">
+          <LangSelect />
         </div>
       </div>
     </div>
