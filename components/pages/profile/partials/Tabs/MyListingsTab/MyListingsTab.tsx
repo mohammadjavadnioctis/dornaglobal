02048f3c0a6dd4228e1a7f69eb11bd23de9897a8
mohammadjavadnioctis <mohammadjavadnoctis2@gmail.com>
2022-10-16 17:46:16 +0300

    feat: ListYourPropertyButton to Mylistings tab in profile page

--- components/pages/profile/partials/Tabs/MyListingsTab/MyListingsTab.tsx
+++ components/pages/profile/partials/Tabs/MyListingsTab/MyListingsTab.tsx
@@ -1,24 +1,28 @@
-import React, {FC, memo} from 'react'
-import NoResults from '~/components/NoResult/NoResult'
-import { isDev } from '~/utils/helpers'
+import React, { FC, memo } from "react";
+import ListyourPropertyButton from "~/components/ListYourPropertyButton/ListyourPropertyButton";
+import NoResults from "~/components/NoResult/NoResult";
+import { isDev } from "~/utils/helpers";
 
-const MyListingsTab:FC = memo(
-
-    () => {
-      return (
-        <div className='w-full h-full p-4'>
-        <h2 className='text-accent-600'>My Listings</h2>
-        <h3 className='text-sm opacity-60'> all Listings </h3>
-        <div className='mt-7'>
-            <NoResults />
+const MyListingsTab: FC = memo(() => {
+  return (
+    <div className="w-full h-full p-4">
+      <div className="w-full flex justify-between">
+        <div>
+          <h2 className="text-accent-600">My Listings</h2>
+          <h3 className="text-sm opacity-60"> all Listings </h3>
         </div>
-        </div>
-      )
-    }
-)
 
-if(isDev){
-    MyListingsTab.displayName = 'MyListingsTab'
+        <ListyourPropertyButton />
+      </div>
+      <div className="mt-7">
+        <NoResults />
+      </div>
+    </div>
+  );
+});
+
+if (isDev) {
+  MyListingsTab.displayName = "MyListingsTab";
 }
 
-export default MyListingsTab
+export default MyListingsTab;
