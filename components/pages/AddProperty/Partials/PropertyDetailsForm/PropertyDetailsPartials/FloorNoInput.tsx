7808d74b92f9c223b51e2b9a27ee4a3e0cc38b5e
Saye <mohammadjavadnoctis2@gmail.com>
2022-11-28 23:07:46 +0300

    FloorNo and NoOfBathrooms connected to context

--- components/pages/AddProperty/Partials/PropertyDetailsForm/PropertyDetailsPartials/FloorNoInput.tsx
+++ components/pages/AddProperty/Partials/PropertyDetailsForm/PropertyDetailsPartials/FloorNoInput.tsx
@@ -1,9 +1,16 @@
 import React, { FC, memo, useState } from "react";
+import { usePropertyContext } from "~/contexts/AddPropertyContext";
 import { UiNumberInput } from "~/lib";
 import { isDev } from "~/utils/helpers";
 
 const FloorNoInput: FC = memo(() => {
-  const [floorNo, setFloorNo] = useState<number>();
+  // const [floorNo, setFloorNo] = useState<number>();
+
+  const {details: {noOfBedRooms}, setDetails} = usePropertyContext()
+
+  const handleChange = (event: number) => {
+    setDetails(prevState => ({...prevState , noOfBathRooms: event}))
+  }
 
   return (
     <div>
@@ -11,8 +18,8 @@ const FloorNoInput: FC = memo(() => {
       <UiNumberInput
         label="Floor number"
         placeholder="which floor is the property located at"
-        value={floorNo}
-        onChange={(val) => setFloorNo(val)}
+        value={noOfBedRooms}
+        onChange={handleChange}
         parser={(value) => value?.replace(/\$\s?|(,*)/g, "")}
       />
     </div>
--- components/pages/AddProperty/Partials/PropertyDetailsForm/PropertyDetailsPartials/NoOfBathroomsInput.tsx
+++ components/pages/AddProperty/Partials/PropertyDetailsForm/PropertyDetailsPartials/NoOfBathroomsInput.tsx
@@ -1,17 +1,24 @@
 import React, { FC, memo, useState } from "react";
+import { usePropertyContext } from "~/contexts/AddPropertyContext";
 import { UiNumberInput } from "~/lib";
 import { isDev } from "~/utils/helpers";
 
 const NoOfBathroomsInput: FC = memo(() => {
-  const [NoOfBathrooms, setNoOfBathrooms] = useState<number>();
+  // const [NoOfBathrooms, setNoOfBathrooms] = useState<number>();
+
+  const {details: {noOfBedRooms}, setDetails} = usePropertyContext()
+
+  const handleChange = (event: number) => {
+    setDetails(prevState => ({...prevState , noOfBathRooms: event}))
+  }
 
   return (
     <div>
       {" "}
       <UiNumberInput
         label="Number of bathrooms"
-        value={NoOfBathrooms}
-        onChange={(val) => setNoOfBathrooms(val)}
+        value={noOfBedRooms}
+        onChange={handleChange}
         parser={(value) => value?.replace(/\$\s?|(,*)/g, "")}
       />
     </div>
