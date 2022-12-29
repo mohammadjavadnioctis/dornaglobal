b86a39e5d922f24367064a4758f7752aba8aec26
Saye <mohammadjavadnoctis2@gmail.com>
2022-12-29 19:57:32 +0300

    feat: aply button to the mobile filters

--- components/pages/Search/Partials/MobileFilters/MobileFilters.tsx
+++ components/pages/Search/Partials/MobileFilters/MobileFilters.tsx
@@ -1,7 +1,8 @@
 import { createStyles } from '@mantine/core'
-import React, {FC, memo, useState} from 'react'
+import React, { FC, memo, useState } from 'react'
 import { MdFilterList } from 'react-icons/md'
 import MultiLevelMenu from '~/components/MultiLevelMenu/MultiLevelMenu'
+import { useSearchProperties } from '~/contexts/SearchPropertiesContext'
 import { UiModal } from '~/lib'
 import { MobileFiltersData } from '~/utils/data'
 
@@ -9,8 +10,11 @@ import { MobileFiltersData } from '~/utils/data'
 const useStyles = createStyles((theme) => ({
     modal: {
         width: '100vw',
-        height: '100vh',
-        
+        height: '80vh',
+    },
+    inner: {
+        height: '100%',
+        position: 'relative'
     }
 }))
 
@@ -18,24 +22,30 @@ const useStyles = createStyles((theme) => ({
 const MobileFilters: FC = memo(
     () => {
         const [opened, setOpened] = useState(false)
-        const {classes} = useStyles()
+        const { classes } = useStyles()
+        const { fetchBasedOnFilters } = useSearchProperties()
 
-     return (
-        <div>
-            <div onClick={() => {setOpened(prevState => !prevState)}} className="w-full flex justify-between items-center py-2 px-2 bg-white rounded-lg  border border-accent my-4"><MdFilterList /> Filters</div>
-            <UiModal
-            opened={opened}
-            onClose={() => setOpened(false)}
-            classNames={{modal: classes.modal}}
-            centered>
-                
 
-        <MultiLevelMenu structure={MobileFiltersData} />
+        return (
+            <div>
+                <div onClick={() => { setOpened(prevState => !prevState) }} className="w-full flex justify-between items-center py-2 px-2 bg-white rounded-lg  border border-accent my-4"><MdFilterList /> Filters</div>
+                <UiModal
+                    opened={opened}
+                    onClose={() => setOpened(false)}
+                    classNames={{ modal: classes.modal, body: classes.inner }}
+                    centered>
 
-            </UiModal>
-        </div>
-     )
-   }
+
+                    <MultiLevelMenu structure={MobileFiltersData} />
+                <div className='absolute bottom-8 w-full '>
+                    <button onClick={()=> {fetchBasedOnFilters()}} className="w-full border bg-accent py-4">Apply</button>
+                    
+                </div>
+                </UiModal>
+
+            </div>
+        )
+    }
 
 )
 
