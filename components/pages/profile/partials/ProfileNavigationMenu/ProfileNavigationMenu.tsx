2f5cdc13e913908c16d207faee7d7d26b9f571ad
MohammedJR <mohammed.j@hotelistan.com>
2022-12-30 10:43:22 +0300

    fix: user dashboard navigation menu styles

--- components/pages/profile/partials/ProfileNavigationMenu/ProfileNavigationMenu.tsx
+++ components/pages/profile/partials/ProfileNavigationMenu/ProfileNavigationMenu.tsx
@@ -1,35 +1,48 @@
+import { createStyles } from '@mantine/core'
 import React, { FC, memo } from 'react'
+import { BiChevronDown } from 'react-icons/bi'
 import { useUserDashboardContext } from '~/contexts/UserDashboardContext'
 import { UiNativeSelect } from '~/lib'
 import { userDashboardMenu } from '~/utils/data/menus'
 import { isDev } from '~/utils/helpers'
 
+
+
+
+const useStyles = createStyles((theme) => ({
+   
+    input: {
+      backgroundColor: "white",
+      borderColor: '#E9C46A',
+      '&:focus': {
+        borderColor: '#E9C46A',
+    },
+    },
+  }));
+  
+
+
 const UserProfileNavigationMenu: FC = memo(
     () => {
         const { activeTab, setActiveTab } = useUserDashboardContext();
-
+        const {classes} = useStyles()
         const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-             const value = e?.currentTarget?.value
-             setActiveTab(value)
+            const value = e?.currentTarget?.value
+            setActiveTab(value)
         }
         const menuOptions = userDashboardMenu.map(menuItem => ({
             value: menuItem.href,
             label: menuItem.name
         }))
         return (
-            <div>
+            <div className='my-4 px-3'>
                 <UiNativeSelect
-                  
-                    value={menuOptions[0]?.value}
+
+                    value={activeTab ?? menuOptions[0]?.value}
                     onChange={handleChange}
-                    // data={[
-                    //     { value: '', label: '' },
-                    //     { value: 'mulkiyet', label: 'Kat Mülkiyet' },
-                    //     { value: 'irtifak', label: 'Kat İrtifaklı' },
-                    //     { value: 'hisseli', label: 'Hisseli' },
-                    //     { value: 'arsa', label: 'Arsa Tapulu' },
-                    // ]}
                     data={menuOptions}
+                    classNames={{input: classes.input}}
+                    rightSection={<BiChevronDown />}
                 />
             </div>
         )
