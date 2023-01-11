93e452e80edb8d6f92be8a0a6af4c5c04fabe5d2
Saye <mohammadjavadnoctis2@gmail.com>
2023-01-11 20:55:34 +0300

    feat: translation for TitleDeedStatusFilterInpu

--- components/pages/Search/Partials/Filters/Partials/TitleDeedStatusFilterInput.tsx
+++ components/pages/Search/Partials/Filters/Partials/TitleDeedStatusFilterInput.tsx
@@ -1,19 +1,20 @@
 import React, {FC, memo} from 'react'
 import { useSearchProperties } from '~/contexts/SearchPropertiesContext'
 import { UiSelect } from '~/lib'
+import useTrans from '~/lib/useTranslate'
 import { TitleDeedTypes } from '~/utils/data'
 import { isDev } from '~/utils/helpers'
 
 const TitleDeedStatusFilterInput: FC = memo(
     () => {
         const { filters, handleFilterchange } = useSearchProperties()
-
+        const t = useTrans()
       return (
         <div className="form-input-wrapper p-4 bg-white rounded-lg border border-accent-200 mb-3">
         <label className="text-xs"> </label>
     
         <div className="form_input_section ">
-          <label className="text-xs">Title deed status:</label>
+          <label className="text-xs">{t("TITLE DEED STATUS")} :</label>
           <UiSelect
             defaultValue={undefined}
             value={filters.titleDeedStatus}
