a5e9dc589961a565d0c350b5e7602abbe7a7060a
Saye <mohammadjavadnoctis2@gmail.com>
2023-01-11 20:52:02 +0300

    feat: translation functionality for FloorNoFilterInput

--- components/pages/Search/Partials/Filters/Partials/FloorNoFilterInput.tsx
+++ components/pages/Search/Partials/Filters/Partials/FloorNoFilterInput.tsx
@@ -1,15 +1,16 @@
 import React, { memo, FC } from 'react'
 import { useSearchProperties } from '~/contexts/SearchPropertiesContext'
 import { UiNumberInput } from '~/lib'
+import useTrans from '~/lib/useTranslate'
 import { isDev } from '~/utils/helpers'
 
 const FloorNoFilterInput: FC = memo(
     () => {
         const { filters, setFilters, fetchBasedOnFilters, handleFilterchange } = useSearchProperties()
-
+        const t = useTrans()
         return (
             <div className="form-input-wrapper p-4 bg-white rounded-lg border border-accent-200 mb-3">
-                <label className="text-xs">Floor no :</label>
+                <label className="text-xs">{t("FLOOR NUMBER")} :</label>
 
                 <UiNumberInput
                     // label="Price"
