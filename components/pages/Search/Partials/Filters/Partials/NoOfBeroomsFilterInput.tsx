9f77f829f263c13e9e0d65575c33b479ee458ea8
Saye <mohammadjavadnoctis2@gmail.com>
2023-01-11 20:45:00 +0300

    feat: translation for BedroomsCount Iniput

--- components/pages/Search/Partials/Filters/Partials/NoOfBeroomsFilterInput.tsx
+++ components/pages/Search/Partials/Filters/Partials/NoOfBeroomsFilterInput.tsx
@@ -1,14 +1,16 @@
 import React, {FC, memo} from 'react'
 import { useSearchProperties } from '~/contexts/SearchPropertiesContext'
 import { UiNumberInput } from '~/lib'
+import useTrans from '~/lib/useTranslate'
 import { isDev } from '~/utils/helpers'
 
 const NoOfBeroomsFilterInput: FC = memo(
     () => {
         const {filters, setFilters, fetchBasedOnFilters, handleFilterchange}  =  useSearchProperties()
+        const t = useTrans()
       return (
         <div className="form-input-wrapper p-4 bg-white rounded-lg border border-accent-200 mb-3">
-        <label className="text-xs">Number of bedrooms :</label>
+        <label className="text-xs">{t("BEDROOM COUNT")} :</label>
 
         <UiNumberInput
           // label="Price"
@@ -22,7 +24,7 @@ const NoOfBeroomsFilterInput: FC = memo(
 )
 
 if(isDev){
-    NoOfBeroomsFilterInput.displayName = 'NNoOfBeroomsFilterInputo'
+    NoOfBeroomsFilterInput.displayName = 'NoOfBeroomsFilterInputo'
 }
 
 export default NoOfBeroomsFilterInput
